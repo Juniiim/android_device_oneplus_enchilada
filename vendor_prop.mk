55453045ef7474696be1c2536ca9a3dae6b0a3e9
Junior Andrade de Almedia <junioraalmeida@hotmail.com>
2019-12-22 14:02:16 -0300

    initial config

---
+++ vendor_prop.mk
@@ -0,0 +1,7 @@
+# OnePlus 6 props
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.mms_notification=free.ogg \
+	ro.config.notification_sound=meet.ogg \
+	ro.config.alarm_alert=spring.ogg \
+	ro.config.ringtone=oneplus_tune.ogg \
+	ro.sf.lcd_density=420
